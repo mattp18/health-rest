cf4de3d519f1653ce699c455f8a51d5cea1920f3
Matt <mpuent18@gmail.com>
2021-04-21 14:30:35 -0400

    initial commit

---
+++ src/main/java/com/code4joe/hosprest/HospRestApplication.java
@@ -0,0 +1,13 @@
+package com.code4joe.hosprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code4joe/hosprest/HospRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4joe.hosprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
